7b18db29648c68d3fd97d83553b6a39bf6cec467
Aisha-Farah <aisha.farah@student.moringaschool.com>
2021-09-20 10:04:14 +0300

    added checkout

--- js/app.js
+++ js/app.js
@@ -1,14 +1,4 @@
-// const totallOrder = [];
-// const orderSummary = document.querySelector("#order-summary");
-// const tot = document.querySelector("#total-price");
-// const prices = {
-//     small : 500,
-//     medium: 700,
-//     large: 1200
-// }
-// let total = 0;
-
-const totallOrder = [];
+const totalOrder = [];
 const orderSummary = $("#order-summary");
 const tot = $("#total-price");
 
@@ -32,7 +22,7 @@ $("form").submit(function(event){
         cost: prices[fd.get("size")]
     }
     total += parseFloat(prices[fd.get("size")])
-    totallOrder.push(orderLine)
+    totalOrder.push(orderLine)
     orderSummary.append(`
         <tr>
             <td>${orderLine.type}</td>
@@ -51,6 +41,19 @@ $("#tbl").on('click', '.btn-remove', function () {
     $(this).closest('tr').remove();
 });
 
+$('#checkout').on('click', () => {
+    if (total === 0) {
+        return alert('Invalid data')
+    } else {
+    prompt("enter your email address");
+    prompt("enter your phone number");
+    prompt("enter your location");
+    alert("Your pizza will be delivered to you within 40mins. Your delivery charge is sh.250. Thank you trusting us.")
+    }
+})
+
+
+
 //     alert("Thank you " + name + "! Your order has been confirmed, Thank you .");
 //     document.getElementById("").reset();
 
@@ -59,3 +62,5 @@ $("#tbl").on('click', '.btn-remove', function () {
 // function popUp() {
 //     alert("Your order is will be with you in 40 mins. Thank you for choosing us")
 // }
+
+
